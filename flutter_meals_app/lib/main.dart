420d90a686630e510833f9cf58299290f16d2bcf
Mohit <mkvmb.6@gmail.com>
2020-05-26 02:11:46 +0530

    Categories and meal item is shown with images. #AB213

--- flutter_meals_app/lib/main.dart
+++ flutter_meals_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'screens/category_meals_screen.dart';
+
+import 'screens/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,10 +14,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
+      routes: {
+        CategoryMealsScreen.route: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
@@ -25,7 +47,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
