14da1b7a77edaed391f37e36495daddf84a328d6
Mohit <mkvmb.6@gmail.com>
2020-05-26 02:11:46 +0530

    Categories and meal item is shown with images. AB#213

---
+++ flutter_meals_app/lib/widgets/category_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'file:///C:/Users/Mohit/AndroidStudioProjects/FlutterProjects/FlutterProjects/flutter_meals_app/lib/screens/category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String _id;
+  final String _title;
+  final Color _color;
+
+  CategoryItem(this._id, this._title, this._color);
+
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      CategoryMealsScreen.route,
+      arguments: {
+        'id': _id,
+        'title': _title,
+      },
+    );
+    /*Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) {
+        return CategoryMealsScreen(_id, _title);
+      }),
+    );*/
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          _title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [_color.withOpacity(0.7), _color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
